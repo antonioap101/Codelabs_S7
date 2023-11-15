7218d1ec46914c5a6b3b0ab43ea9a12d11f65c10
Antonio <antapagon@outlook.com>
2023-11-15 23:06:38 +0000

    Primer Commit: Escribe condicionales en Kotlin

---
+++ app/src/main/java/es/ulpgc/conceptosBasicosKotlin/Condicionales_IF_ELSE.kt
@@ -0,0 +1,20 @@
+package es.ulpgc.conceptosBasicosKotlin
+
+fun trafficLightColors(trafficLightColor: String){
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+}
+fun main(){
+    trafficLightColors("Red")
+    trafficLightColors("Yellow")
+    trafficLightColors("Green")
+    trafficLightColors("Black")
+}
---
+++ app/src/main/java/es/ulpgc/conceptosBasicosKotlin/Condicionales_WHEN.kt
@@ -0,0 +1,32 @@
+package es.ulpgc.conceptosBasicosKotlin
+
+fun trafficLightColors2(trafficLightColor: String){
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
+
+fun valueClassifier(x: Any) {
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+}
+
+fun main(){
+    trafficLightColors2("Red")
+    trafficLightColors2("Yellow")
+    trafficLightColors2("Amber")
+    trafficLightColors2("Green")
+    trafficLightColors2("Black")
+    println()
+    valueClassifier(2)
+    valueClassifier(4)
+    valueClassifier(40)
+    valueClassifier("String Test")
+}
