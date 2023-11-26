c41da8e3433cae98ce6114354d15f520fdf7bfa0
Antonio <antapagon@outlook.com>
2023-11-26 23:09:52 +0000

    ArtSpace App Finished Version

--- ArtSpaceApp/src/main/java/es/ulpgc/diceroller/MainActivity.kt
+++ ArtSpaceApp/src/main/java/es/ulpgc/diceroller/MainActivity.kt
@@ -4,25 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import es.ulpgc.diceroller.ui.theme.ArtSpaceTheme
 
 
@@ -31,7 +25,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ArtSpaceTheme {
-                // A surface container using the 'background' color from the theme
                 ArtApp()
             }
         }
@@ -41,51 +34,95 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtApp() {
-    // Suponiendo que tienes una lista de obras de arte
-    val artworks = listOf(
-        Artwork(R.drawable.jovenperla, "La joven de la perla", "Johannes Vermeer"),
-        Artwork(R.drawable.meninas, "Las meninas", "Diego Velázquez"),
-        Artwork(R.drawable.monalisa, "La Mona Lisa", " Leonardo da Vinci"),
-        Artwork(R.drawable.nocheestrellada, "La noche estrellada", "Vincent van Gogh ")
-    )
-    var currentArtIndex by remember { mutableStateOf(0) }
+    BoxWithConstraints {
+        // Calcula si está en modo horizontal o vertical
+        val isLandscape = maxWidth > maxHeight
 
-    // Crear un composable que muestra la obra de arte actual
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        // Imagen de la obra de arte
-        Image(
-            painter = painterResource(id = artworks[currentArtIndex].imageRes),
-            contentDescription = artworks[currentArtIndex].title
+        // Lista de obras de arte
+        val artworks = listOf(
+            Artwork(R.drawable.jovenperla, "La joven de la perla", "Johannes Vermeer"),
+            Artwork(R.drawable.meninas, "Las meninas", "Diego Velázquez"),
+            Artwork(R.drawable.monalisa, "La Mona Lisa", "Leonardo da Vinci"),
+            Artwork(R.drawable.nocheestrellada, "La noche estrellada", "Vincent van Gogh")
         )
+        var currentArtIndex by remember { mutableStateOf(0) }
 
-        // Título y autor de la obra de arte
-        Text(text = artworks[currentArtIndex].title)
-        Text(text = "by ${artworks[currentArtIndex].author}",
-            style = TextStyle(fontWeight = FontWeight.Bold)
-        )
+        // Layout de la aplicación
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Imagen de la obra de arte
+            Image(
+                painter = painterResource(id = artworks[currentArtIndex].imageRes),
+                contentDescription = artworks[currentArtIndex].title,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+            )
 
-        // Botones para navegar a la siguiente y anterior obra de arte
-        Row {
-            Button(onClick = {
-                if (currentArtIndex > 0) currentArtIndex--
-            }) {
-                Text(text = "Previous")
+            // Caja para el texto con color de fondo
+            Surface(
+                modifier = Modifier.padding(4.dp).fillMaxWidth(),
+                color = Color(0xFFEEEEEE) // Color gris claro
+            ) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    // Título de la obra de arte
+                    Text(
+                        text = artworks[currentArtIndex].title,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = if (isLandscape) 24.sp else 18.sp // Tamaño de texto diferente en modo horizontal
+                        )
+                    )
+                    // Autor de la obra de arte
+                    Text(
+                        text = "by ${artworks[currentArtIndex].author}",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = if (isLandscape) 20.sp else 14.sp // Tamaño de texto diferente en modo horizontal
+                        )
+                    )
+                }
             }
-            Spacer(modifier = Modifier.width(18.dp))
-            Button(onClick = {
-                if (currentArtIndex < artworks.size - 1) currentArtIndex++
-            }) {
-                Text(text = "Next")
+
+            // Botones para navegar
+            if (isLandscape) Spacer(modifier = Modifier.height(8.dp)) // Espacio adicional en modo horizontal
+
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // Botón 'Previous' alineado a la izquierda
+                Button(
+                    onClick = {
+                        if (currentArtIndex > 0) currentArtIndex--
+                    },
+                    modifier = Modifier.align(Alignment.CenterStart) // Alinea el botón al inicio (izquierda)
+                ) {
+                    Text(text = "Previous", style = TextStyle(fontSize = 18.sp))
+                }
+
+                // Botón 'Next' alineado a la derecha
+                Button(
+                    onClick = {
+                        if (currentArtIndex < artworks.size - 1) currentArtIndex++
+                    },
+                    modifier = Modifier.align(Alignment.CenterEnd) // Alinea el botón al final (derecha)
+                ) {
+                    Text(text = "Next", style = TextStyle(fontSize = 18.sp))
+                }
             }
         }
     }
 }
 
-// Definir una clase para las obras de arte
+
+// Clase para definir las obras de arte
 data class Artwork(val imageRes: Int, val title: String, val author: String)
 
 
